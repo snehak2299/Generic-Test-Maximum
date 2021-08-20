fcd0aa2ac4dd98395b681e1647f436c114158ad4
snehak2299 <skanase2299@gmail.com>
2021-08-20 16:51:45 +0530

    [sneha]:added maximun integer amoung 3

--- GenericMaximum.java
+++ GenericMaximum.java
@@ -2,7 +2,21 @@ package com.bridglab.corejava;
 
 public class GenericMaximum {
 	public static void main(String[] args) {
+		Integer number1=10, number2=10, number3=10;
+		int max = number1.compareTo(number2);
+		int max1 = number2.compareTo(number3);
+		if(max==1 && max1<0) {
+			System.out.println(number1+ " is maximum number");
+		}
+		else if (max==0 && max1==0) {
+			System.out.println(number1 +","+ number2+","+ number3+" are equal");
+		}
+		else if(max<1 && max1==1) {
+			System.out.println(number2 +" is maximum number");
+		}
+		else if(max<1 && max1<1)
+			System.out.println(number3 +" is maximum number");
+			
 		
-	}
-	
+	}	
 }
